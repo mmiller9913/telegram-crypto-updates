a5e6f40adfe71ea10db71b673d4a8253c4e623bf
Matt Miller <mmiller9913@gmail.com>
2021-10-06 21:57:00 -0400

    testing heroku scheduler

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 // console.log(__dirname);
 // console.log(__filename);
 
-const cron = require ('node-cron');
-cron.schedule('* * * * *', function() { //runs every minute
-    console.log('running a task every minute');
-  });
+// const cron = require ('node-cron');
+// cron.schedule('* * * * *', function() { //runs every minute
+//     console.log('running a task every minute');
+//   });
+
+console.log("Testing Heroku scheduler");
