1ce9d83efbf1d2c8de52392c236debc3111de443
Matt Miller <mmiller9913@gmail.com>
2021-10-09 16:07:07 -0400

    merged app.js & start.js

--- start.js
+++ start.js
@@ -1,6 +1,34 @@
 require('dotenv').config({ path: '.env' });
 
-const app = require('./app');
+const express = require('express')
+const bodyParser = require('body-parser');
+const ejs = require('ejs');;
+
+//Init the app (create the express app);
+const app = express();
+
+//Template engine setup
+app.set('view engine', 'html');
+app.engine('html', ejs.renderFile);
+
+//Public folder setup
+app.use(express.static(__dirname + '/public'));
+
+//Body parser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//index route
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+//to keep nodejs app running indefinitely on heroku
+var http = require("http");
+setInterval(function () {
+    http.get("http://telegram-crypto-updates.herokuapp.com");
+}, 60000); // run every 1 minute
+
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
   console.log(`App started: Express running → PORT ${server.address().port}`);
