3a2b2e5cd73b624b661fc26c0f60bd46d9a2039b
Matt Miller <mmiller9913@gmail.com>
2021-10-09 14:56:28 -0400

    testing

--- start.js
+++ start.js
@@ -3,5 +3,5 @@ require('dotenv').config({ path: '.env' });
 const app = require('./app');
 app.set('port', process.env.PORT || 3000);
 const server = app.listen(app.get('port'), () => {
-    console.log(`App started: Express running → PORT ${server.address().port}`);
-  });
+  console.log(`App started: Express running → PORT ${server.address().port}`);
+});
